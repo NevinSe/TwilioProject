b4e4a8c2236e96d13b217adfbc37aa81443b1448
Sam <SSUrbancic@outlook.com>
2018-08-03 14:00:50 -0500

    EventUserController
    Added an EventUserController to build functionality for Host and Attendees as well as a loction to return the Views built for both. Created a ActionResult for AttendeeEntryPortal.

---
+++ TwilioProject/TwilioProject/Controllers/EventUsersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TwilioProject.Controllers
+{
+    public class EventUsersController : Controller
+    {
+        // GET: EventUsers
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AttendeeEntryPortal()
+        {
+            return View();
+        }
+    }
+}
