789128f8c01497f92f4ace8f4cdd2aed75c082fe
buck-nicholas <buck.nicholas.c@gmail.com>
2018-08-03 15:04:25 -0500

    Revert "Merge branch 'master' of https://github.com/NevinSe/TwilioProject"
    This reverts commit 4c5f51e95eabbf364e9e11bff11a7fc6d289ff6a, reversing
    changes made to e6c4caaa3ad8088077994a3a09c1ac930425deee.

--- TwilioProject/TwilioProject/Controllers/EventUsersController.cs
+++ TwilioProject/TwilioProject/Controllers/EventUsersController.cs
@@ -9,7 +9,6 @@ namespace TwilioProject.Controllers
     public class EventUsersController : Controller
     {
         // GET: EventUsers
-<<<<<<< HEAD
         public ActionResult Index()
         {
             return View();
@@ -20,7 +19,5 @@ namespace TwilioProject.Controllers
           //  return RedirectToRoute("Attendee", "AttendeeEntryPortal");
            // return RedirectToAction("AttendeeEntryPortal", "EventUsersController");
         }
-=======
->>>>>>> 3384397327e49677ca7eaf61118bf148b6c59eda
     }
 }
