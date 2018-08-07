5858e971bf9e474e3a8daa32dd1db86e1f2e9f94
buck-nicholas <buck.nicholas.c@gmail.com>
2018-08-07 11:48:11 -0500

    Rebuilding Repository
    Adding Files Back

---
+++ TwilioProject/TwilioProject/Models/Playlist.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TwilioProject.Models
+{
+    public class Playlist
+    {
+        [Key]
+        public int SongOrderID { get; set; }
+        public int SongLength { get; set; }
+        public string YoutubeID { get; set; }
+        public string Title { get; set; }
+        public int PhoneNumber { get; set; }
+    }
+}
